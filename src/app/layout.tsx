6b1e1ff0dcb72abfe2424fe210113094badf6ee6
logesh2496 <logesh.r24@gmail.com>
2025-10-28 13:34:57 +0530

    fix: build err

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -28,8 +28,8 @@ export default function RootLayout({
         />
       </head>
       <body className="antialiased">
-        {children}
-        {/* <Providers>{children}</Providers> */}
+        {/* {children} */}
+        <Providers>{children}</Providers>
       </body>
     </html>
   );
--- src/components/providers.tsx
+++ src/components/providers.tsx
@@ -3,36 +3,36 @@
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { PrivyProvider } from "@privy-io/react-auth";
 import { useState } from "react";
-// import { base } from "viem/chains";
 import { toSolanaWalletConnectors } from "@privy-io/react-auth/solana";
 
 export function Providers({ children }: { children: React.ReactNode }) {
   const [queryClient] = useState(() => new QueryClient());
 
   return (
-    <PrivyProvider
-      appId={process.env.NEXT_PUBLIC_PRIVY_APP_ID as string}
-      clientId={process.env.NEXT_PUBLIC_CLIENT_ID as string}
-      config={{
-        // Configure supported chains including Solana
-        supportedChains: [],
-        // Configure login methods
-        loginMethods: ["wallet"],
-        externalWallets: {
-          solana: {
-            connectors: toSolanaWalletConnectors() as any,
-          },
-        },
-        // Appearance configuration
-        appearance: {
-          walletChainType: "solana-only",
-          theme: "dark",
-          accentColor: "#8B5CF6",
-          logo: "https://firebasestorage.googleapis.com/v0/b/lustrous-stack-453106-f6.firebasestorage.app/o/logo1.png?alt=media",
-        },
-      }}
-    >
-      <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
-    </PrivyProvider>
+    <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
+    // <PrivyProvider
+    //   appId={process.env.NEXT_PUBLIC_PRIVY_APP_ID as string}
+    //   clientId={process.env.NEXT_PUBLIC_CLIENT_ID as string}
+    //   config={{
+    //     // Configure supported chains including Solana
+    //     supportedChains: [],
+    //     // Configure login methods
+    //     loginMethods: ["wallet"],
+    //     externalWallets: {
+    //       solana: {
+    //         connectors: toSolanaWalletConnectors() as any,
+    //       },
+    //     },
+    //     // Appearance configuration
+    //     appearance: {
+    //       walletChainType: "solana-only",
+    //       theme: "dark",
+    //       accentColor: "#8B5CF6",
+    //       logo: "https://firebasestorage.googleapis.com/v0/b/lustrous-stack-453106-f6.firebasestorage.app/o/logo1.png?alt=media",
+    //     },
+    //   }}
+    // >
+
+    // </PrivyProvider>
   );
 }
